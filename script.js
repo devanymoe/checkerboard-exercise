f071dc91c4f11ca880733b9184a5ee46392db9c0
Devany Moe <devanymoe@gmail.com>
2016-02-20 15:36:01 -0800

    Make random-colored checkerboard

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 // Your JS goes here
 var body = document.getElementsByTagName('body')[0];
 
+var color = function(){
+  var r = Math.floor(Math.random() * 255);
+  var g = Math.floor(Math.random() * 255);
+  var b = Math.floor(Math.random() * 255);
+  return 'rgba(' + r + ',' + g + ',' + b + ',1)';
+}
+
 for (i = 0; i <= 80; i++){
   var divies = document.createElement('div');
   divies.style.width = '11.1%';
   divies.style.float = 'left';
   divies.style.paddingBottom = '11.1%';
+  divies.style.backgroundColor = color();
   body.appendChild(divies);
-}
-
-var divies = document.getElementsByTagName('div')
-
-for (i = 0; i <= 80; i = i + 2){
-  divies[i].className = "odd";
-  divies[i].style.backgroundColor = 'black';
-}
-
-for (i = 1; i <= 80; i = i + 2){
-  divies[i].className = "even";
-  divies[i].style.backgroundColor = 'red';
 }
