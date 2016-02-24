680b5fc4f97fad152b54d1b8d1aa25f59fa1636d
Devany Moe <devanymoe@gmail.com>
2016-02-24 11:45:20 -0800

    Linting updates

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 // Your JS goes here
-var body = document.getElementsByTagName('body')[0];
+(function() {
+  'use strict';
+  var body = document.getElementsByTagName('body')[0];
 
-for (i = 0; i <= 80; i++){
-  var divies = document.createElement('div');
-  divies.style.width = '11.1%';
-  divies.style.float = 'left';
-  divies.style.paddingBottom = '11.1%';
-  body.appendChild(divies);
-}
+  for (var i = 0; i <= 80; i++) {
+    var divies = document.createElement('div');
 
-setInterval(function() {
-  for (i = 0; i <= 80; i++){
-    var divies = document.getElementsByTagName('div');
-    var divs = divies[i];
-    var r = Math.floor(Math.random() * 255);
-    var g = Math.floor(Math.random() * 255);
-    var b = Math.floor(Math.random() * 255);
-    var colors = 'rgba(' + r + ',' + g + ',' + b + ',1)';
-    divs.style.backgroundColor = colors;
+    divies.style.width = '11.1%';
+    divies.style.float = 'left';
+    divies.style.paddingBottom = '11.1%';
+    body.appendChild(divies);
   }
-}, 2000);
 
-var song = document.createElement('audio');
-body.appendChild(song);
-song.src = "http://devany.moe/images/Crimewave.mp3";
-song.autoplay = "autoplay";
+  setInterval(function() {
+    for (var x = 0; x <= 80; x++) {
+      var allDivs = document.getElementsByTagName('div');
+      var divs = allDivs[x];
+      var rs = Math.floor(Math.random() * 255);
+      var gs = Math.floor(Math.random() * 255);
+      var bs = Math.floor(Math.random() * 255);
+      var colors = 'rgba(' + rs + ',' + gs + ',' + bs + ',1)';
+
+      divs.style.backgroundColor = colors;
+    }
+  }, 2000);
+
+  var song = document.createElement('audio');
+
+  body.appendChild(song);
+  song.src = 'http://devany.moe/images/Crimewave.mp3';
+  song.autoplay = 'autoplay';
+})();
